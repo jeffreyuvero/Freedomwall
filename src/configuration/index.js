dfb8e61377be3981c428c51bb33db6c4eb084947
Jeffrie <jeffrey.uvero@amellar.com>
2018-11-27 15:25:57 +0800

    change configuration settings

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,6 +1,5 @@
 export const Title = "Freedom Wall";  
 
-export const isProduction = 1; 
+export const isProduction = 0; 
 
-// export const API = "http://www.freedomwall.ga/index.php/fwall/"; 
-export const API = "http://192.168.0.54/fwall/index.php/fwall/";
+export const API = "http://www.freedomwall.ga/index.php/fwall/"; 
