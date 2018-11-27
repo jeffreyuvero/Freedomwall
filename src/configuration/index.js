ba498c4461b35617ae4f8a602eace8016b1792d1
Jeffrie <jeffrey.uvero@amellar.com>
2018-11-27 11:41:45 +0800

    put config to not display the redux webtool and toggle API.

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,1 +1,6 @@
 export const Title = "Freedom Wall";  
+
+export const isProduction = 1; 
+
+// export const API = "http://www.freedomwall.ga/index.php/fwall/"; 
+export const API = "http://192.168.0.54/fwall/index.php/fwall/";
